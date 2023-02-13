eafe72d8dd19c8f207f3535e18f9baeb98f857ee
yiyi1231 <yiyi1231@yiyi1231deMacBook-Pro.local>
2023-02-13 14:44:30 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import { h as ec, l as tc, f as fm, m as ha, z as za, o as Ki, q as Qi, s as sa, D as Di, v as Hi, w as Zi } from '../profile-hook-39ba154a.js';
+import { U as UO } from '../App-109b9f30.js';
+
+function f(t){za(t,"svelte-saji4y","body{height:100%;width:100%}:root{--drawer-width:256px;--app-content-width:calc(100% - 260px);--top-bar-width:calc(100% - 255px);--profiles-list-min-height:inherit}");}function h(t){let s,a;return s=new UO({props:{isFullscreen:!0}}),{c(){Ki(s.$$.fragment);},m(t,e){Qi(s,t,e),a=!0;},p:sa,i(t){a||(Di(s.$$.fragment,t),a=!0);},o(t){Hi(s.$$.fragment,t),a=!1;},d(t){Zi(s,t);}}}fm();const m=new class extends ec{constructor(t){super(),tc(this,t,null,h,ha,{},f);}}({target:document.body});
+
+export { m as default };
---
+++ js/popup.js
@@ -0,0 +1,6 @@
+import { h as ec, l as tc, f as fm, m as ha, z as za, o as Ki, q as Qi, s as sa, D as Di, v as Hi, w as Zi } from '../profile-hook-39ba154a.js';
+import { U as UO } from '../App-109b9f30.js';
+
+function h(t){za(t,"svelte-12xdgf","body{height:500px;width:620px !important}:root{--drawer-width:36px;--app-content-width:580px;--top-bar-width:585px;--profiles-list-min-height:360px}");}function c(t){let a,s;return a=new UO({}),{c(){Ki(a.$$.fragment);},m(t,o){Qi(a,t,o),s=!0;},p:sa,i(t){s||(Di(a.$$.fragment,t),s=!0);},o(t){Hi(a.$$.fragment,t),s=!1;},d(t){Zi(a,t);}}}fm();const l=new class extends ec{constructor(t){super(),tc(this,t,null,c,ha,{},h);}}({target:document.body});
+
+export { l as default };
